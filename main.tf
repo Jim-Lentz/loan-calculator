f59dd5665540b28776fccc2a1a528e2882c2845e
Jim Lentz <jim_lentz@outlook.com>
2024-03-21 20:26:30 -0700

    added resource group

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
 provider "azurerm" {
   features {}
 }
+
+resource "azurerm_resource_group" "example" {
+  name     = "dev-rgp-cis-neubank-use-001"
+  location = "East US"
+
+  tags = {
+    Environment = "Dev"
+    Owner = "first.last@company.com"
+    Project = "Mortgage Calculator"
+  }
+}
