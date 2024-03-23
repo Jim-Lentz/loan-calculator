8ec0987b991331f80fc840e3ac5fdcf108495c1d
Jim Lentz <jim_lentz@outlook.com>
2024-03-23 09:10:47 -0700

    adding remote state

--- main.tf
+++ main.tf
@@ -5,6 +5,13 @@
       version = "=3.0.0"
     }
   }
+    backend "azurerm" {
+      resource_group_name  = "tfstate"
+      storage_account_name = "tfstateyyrxc"
+      container_name       = "tfstate"
+      key                  = "terraform.tfstate"
+  }
+
 }
 
 provider "azurerm" {
