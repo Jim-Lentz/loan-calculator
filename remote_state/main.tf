9ea14f409617ef794421504ad29850075837e4bf
Jim Lentz <jim_lentz@outlook.com>
2024-04-07 20:23:31 -0700

    checkov skip subdir

--- remote_state/main.tf
+++ remote_state/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>3.0"
+      version = "3.97.1"
     }
   }
 }
@@ -27,14 +27,41 @@ resource "azurerm_storage_account" "tfstate" {
   resource_group_name      = azurerm_resource_group.tfstate.name
   location                 = azurerm_resource_group.tfstate.location
   account_tier             = "Standard"
-  account_replication_type = "LRS"
+  account_replication_type = "GRS"
+#  public_network_access_enabled = false
   allow_nested_items_to_be_public = false
+ # shared_access_key_enabled = false
+  blob_properties {
+    delete_retention_policy {
+      days = 7
+    }
+  }
+  sas_policy {
+    expiration_period = "90.00:00:00"
+    expiration_action = "Log"
+  }
+
 
     tags = {
     Owner = "first.last@company.com"
     Project = "Mortgage Calculator"
   }
 }
+/*
+resource "azurerm_private_endpoint" "tfendpoint" {
+  name                 = "tf_private_endpoint"
+  location             = azurerm_resource_group.tfstate.location
+  resource_group_name  = azurerm_resource_group.tfstate.name
+  subnet_id            = azurerm_subnet.example.id
+
+  private_service_connection {
+    name                           = "tf_psc"
+    is_manual_connection           = false
+    private_connection_resource_id = azurerm_storage_account.tfstate.id
+    subresource_names              = ["blob"]
+  }
+}
+*/
 
 resource "azurerm_storage_container" "tfstate" {
   name                  = "tfstate"
